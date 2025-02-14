9759eb062e5125317d116592aef65d33e0ef0615
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2025-02-14 17:57:58 +0530

    complete feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,100 @@
 import { useState } from "react";
-import Modal from "./Components/Modal";
 import { images } from "./Utils/imagesData";
 
-function App() { 
+function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [index, setIndex] = useState(0);
 
-  const [showModal, setShowModal]= useState(false);
-  const [modalImg, setModalImg]= useState(null);
+  const openModal = (imgIndex) => {
+    setIndex(imgIndex);
+    setSelectedImage(images[imgIndex]);
+  };
 
-  const handleImgClick=(image)=>{
-    console.log(image.id);
-    setShowModal(true);
-    setModalImg(image)
-    console.log(modalImg);
-    
-  }
+  const closeModal = () => setSelectedImage(null);
+  const prevImage = () =>
+    setIndex((prev) => (prev > 0 ? prev - 1 : images.length - 1));
+  const nextImage = () =>
+    setIndex((prev) => (prev < images.length - 1 ? prev + 1 : 0));
 
   return (
     <>
-      <h1 className="text-center text-5xl my-4">Image Gallery</h1>
-      {showModal && modalImg && <Modal image={modalImg} setShowModal={setShowModal}/> }
-      <div className="flex flex-wrap justify-center">
-          {images.map((i)=><img key={i.id} src={i.url} alt="wallpaper Image" className="w-1/4 m-2 p-2 hover:scale-105 border border-slate-400 rounded-xl cursor-pointer" onClick={()=>handleImgClick(i)}/> )}
-      </div>
-      
+      {!selectedImage && (
+        <>
+          {" "}
+          <h1 className="text-center text-3xl md:text-5xl my-4 font-extrabold ">
+            Image Gallery
+          </h1>
+          <div className="w-full flex flex-wrap justify-center">
+            {images.map((src, i) => (
+              <img
+                key={i}
+                src={src}
+                alt="wallpaper Image"
+                className="w-[130px] h-[100px] md:w-[320px] md:h-[240px] m-2 md:m-4 p-1 md:p-2 bg-white hover:scale-105 border border-slate-400 rounded-xl cursor-pointer shadow-xl"
+                onClick={() => openModal(i)}
+              />
+            ))}
+          </div>
+        </>
+      )}
+
+      {selectedImage && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="p-4">
+            <img
+              src={images[index]}
+              alt="Selected"
+              className="md:max-w-[60%] md:max-h-[80%] mx-auto rounded-lg object-contain"
+            />
+          </div>
+          <button
+            className="absolute top-5 right-5 text-white cursor-pointer hover:bg-red-600 hover:scale-125"
+            onClick={closeModal}
+          >
+            ✖
+          </button>
+          <button
+            className="absolute left-4 top-1/2 text-white cursor-pointer hover:scale-150 bg-black opacity-60"
+            onClick={prevImage}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 md:size-10"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+          <button
+            className="absolute right-4 top-1/2 text-white cursor-pointer hover:scale-150 bg-black opacity-60"
+            onClick={nextImage}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 md:size-10"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Utils/imagesData.js
+++ src/Utils/imagesData.js
@@ -1,26 +1,21 @@
 export const images=[
-    {
-      id:1,
-      url:"https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg?auto=compress&cs=tinysrgb"
-    },
-    {
-      id:2,
-      url:"https://images.pexels.com/photos/1261728/pexels-photo-1261728.jpeg?auto=compress&cs=tinysrgb"
-    },
-    {
-      id:3,
-      url:"https://images.pexels.com/photos/983200/pexels-photo-983200.jpeg?auto=compress&cs=tinysrgb"
-    },
-    {
-      id:4,
-      url:"https://images.pexels.com/photos/772803/pexels-photo-772803.jpeg?auto=compress&cs=tinysrgb"
-    },
-    {
-      id:5,
-      url:"https://images.pexels.com/photos/35857/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb"
-    },
-    {
-        id:6,
-        url:"https://images.pexels.com/photos/1275415/pexels-photo-1275415.jpeg?auto=compress&cs=tinysrgb"
-    }
+   "https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg?auto=compress&cs=tinysrgb&",
+
+    "https://images.pexels.com/photos/1261728/pexels-photo-1261728.jpeg?auto=compress&cs=tinysrgb",
+
+    "https://images.pexels.com/photos/35857/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb",
+
+    "https://images.pexels.com/photos/355288/pexels-photo-355288.jpeg?auto=compress&cs=tinysrgb",
+
+    "https://images.pexels.com/photos/772803/pexels-photo-772803.jpeg?auto=compress&cs=tinysrgb",
+
+
+    "https://images.pexels.com/photos/1275415/pexels-photo-1275415.jpeg?auto=compress&cs=tinysrgb",
+
+    "https://images.pexels.com/photos/1435752/pexels-photo-1435752.jpeg?auto=compress&cs=tinysrgb",
+
+    "https://images.pexels.com/photos/844297/pexels-photo-844297.jpeg?auto=compress&cs=tinysrgb",
+
+    "https://images.pexels.com/photos/1198817/pexels-photo-1198817.jpeg?auto=compress&cs=tinysrgb"
+  
   ]
